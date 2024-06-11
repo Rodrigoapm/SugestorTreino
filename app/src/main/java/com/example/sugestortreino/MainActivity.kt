73c05beab5683d33b092a00c244aa38ac7772174
Rodrigo Martins <rodasapm@hotmail.com>
2024-06-11 10:25:21 +0100

    configuraçao da pagina inicial

--- app/src/main/java/com/example/sugestortreino/MainActivity.kt
+++ app/src/main/java/com/example/sugestortreino/MainActivity.kt
@@ -1,47 +1,22 @@
-package com.example.sugestortreino
+package com.seu_nome.futsaltrainer
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sugestortreino.ui.theme.SugestorTreinoTheme
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sugestortreino.R
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            SugestorTreinoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+
+        btnStart.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SugestorTreinoTheme {
-        Greeting("Android")
     }
 }
